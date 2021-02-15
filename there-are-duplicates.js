3c8ae3808723ad42479cdcbea75e3b8854859e9e
Yandro Portal <suarez.portal@gmail.com>
2021-02-15 00:30:42 -0600

    Update there-are-duplicates.js

--- there-are-duplicates.js
+++ there-are-duplicates.js
@@ -1,7 +1,7 @@
 // create a function to find if there are duplicates among arguments passed.
 //time complexity O(n)
 
-function thereDups(arg){
+function thereDups(....arg){
 
     if(!arg.length) return false;
 
