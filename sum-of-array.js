8fbb40eb316ad01790a723fa4543376bc374088b
Yandro Portal <suarez.portal@gmail.com>
2021-02-15 11:11:41 -0600

    Add files via upload

---
+++ sum-of-array.js
@@ -0,0 +1,14 @@
+//find the sum of an array
+//time complexity O(n)
+
+function sumOfArray(arr){
+
+    const sum = 0;
+
+    for(let i of arr){
+        sum +=arr[i];
+    }
+
+    return sum;
+
+}
