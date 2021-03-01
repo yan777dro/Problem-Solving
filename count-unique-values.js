c3c37c2797447934b0b0b915c0ff8fb32030ff7c
Yandro Portal <suarez.portal@gmail.com>
2021-02-28 21:09:15 -0600

    Add files via upload

---
+++ count-unique-values.js
@@ -0,0 +1,19 @@
+//count unique pairs on a sorted array
+//time complexity O(n)
+
+function CountUniqueValues(arr){
+
+    if (!arr.length) return 0;
+
+    const count = 1;
+
+    for(index of arr){
+
+        if(arr[index] == arr[index + 1]) return count;
+
+
+    }
+
+    count++;
+
+}
