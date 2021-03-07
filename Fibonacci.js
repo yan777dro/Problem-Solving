dde91c1156cbb30d27d1d8f9d1b0776e429e5937
Yandro Portal <suarez.portal@gmail.com>
2021-03-06 18:56:22 -0600

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -32,7 +32,7 @@ in an array. **/
        
     }
     
-    //console.log(FibonacciSeq(20));
+    console.log(FibonacciSeq(20));
     
     //[1, 1 , 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765];
     
